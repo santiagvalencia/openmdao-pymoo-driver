78899f587ad49113ef6347ee0694089137c91670
Santiago Valencia <42377578+santiagvalencia@users.noreply.github.com>
2025-05-13 16:05:57 -0400

    Add sphere problem (single objective) example with several Pymoo algorithms

---
+++ examples/sphere_problem.py
@@ -0,0 +1,71 @@
+import openmdao.api as om
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.algorithms.soo.nonconvex.de import DE
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.algorithms.soo.nonconvex.nelder import NelderMead, NelderAndMeadTermination
+from pymoo.core.algorithm import Algorithm
+
+from pymoo_driver.driver import PymooDriver
+
+
+class SphereComp(om.ExplicitComponent):
+    def setup(self):
+        self.add_input("x", val=0.0)
+        self.add_input("y", val=0.0)
+        self.add_output("f", val=0.0)
+        self.add_output("g", val=0.0)
+
+    def setup_partials(self):
+        self.declare_partials("*", "*", method="fd")
+
+    def compute(self, inputs, outputs):
+        x = inputs["x"]
+        y = inputs["y"]
+        outputs["f"] = x**2 + y**2
+        outputs["g"] = 1 - x - y
+
+
+def make_and_run_problem(algorithm: Algorithm):
+    prob = om.Problem()
+    model = prob.model
+
+    model.add_subsystem("sphere", SphereComp(), promotes=["*"])
+    prob.driver = PymooDriver()
+    prob.driver.options["algorithm"] = algorithm
+    prob.driver.options["termination"] = algorithm.termination
+    prob.driver.options["verbose"] = False
+
+    model.add_design_var("x", lower=-2.0, upper=2.0)
+    model.add_design_var("y", lower=-2.0, upper=2.0)
+    model.add_objective("f")
+    model.add_constraint("g", upper=0.0)
+
+    prob.setup()
+    prob.set_val("x", 0.5)
+    prob.set_val("y", 0.5)
+
+    prob.run_driver()
+
+    print(
+        f"Best solution found using {algorithm.__repr__().split(' ')[0].split('.')[-1]}:"
+    )
+    print(f"x = {prob.get_val('x')}")
+    print(f"y = {prob.get_val('y')}")
+    print(f"f = {prob.get_val('f')}")
+    print(f"g = {prob.get_val('g')}")
+
+
+def run():
+    algorithms = [
+        NSGA2(pop_size=40, termination=("n_gen", 10)),
+        DE(pop_size=40, termination=("n_gen", 10)),
+        GA(pop_size=40, termination=("n_gen", 10)),
+        NelderMead(termination=NelderAndMeadTermination()),
+    ]
+
+    for algorithm in algorithms:
+        make_and_run_problem(algorithm)
+
+
+if __name__ == "__main__":
+    run()
