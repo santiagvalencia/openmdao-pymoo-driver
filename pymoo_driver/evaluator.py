433a936ff70ef436add225ce212a3d5cc0b82a75
Santiago Valencia <42377578+santiagvalencia@users.noreply.github.com>
2025-05-13 16:29:57 -0400

    Add openmdao.core.Driver type hint

--- pymoo_driver/evaluator.py
+++ pymoo_driver/evaluator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from openmdao.core.driver import Driver
 
 
 class OpenMDAOEvaluator:
@@ -6,7 +7,7 @@ class OpenMDAOEvaluator:
     Wraps an OpenMDAO problem so that it can be evaluated by pymoo.
     """
 
-    def __init__(self, driver):
+    def __init__(self, driver: Driver):
         self.driver = driver
 
     def set_design_vars(self, x):
@@ -14,7 +15,7 @@ class OpenMDAOEvaluator:
         i = 0
         for name, meta in self.driver._designvars.items():
             size = meta["size"]
-            self.driver.set_design_var(name, x[i:i + size])
+            self.driver.set_design_var(name, x[i : i + size])
             i += size
 
     def evaluate(self, x):
@@ -30,12 +31,16 @@ class OpenMDAOEvaluator:
             nG = len(self.driver._cons)
             return np.full(nF, 1e20), np.full(nG, 1e20)
 
-        F = np.array([
-            self.driver._get_voi_val(name, meta, self.driver._remote_objs)
-            for name, meta in self.driver._objs.items()
-        ])
-        G = np.array([
-            self.driver._get_voi_val(name, meta, self.driver._remote_cons)
-            for name, meta in self.driver._cons.items()
-        ])
+        F = np.array(
+            [
+                self.driver._get_voi_val(name, meta, self.driver._remote_objs)
+                for name, meta in self.driver._objs.items()
+            ]
+        )
+        G = np.array(
+            [
+                self.driver._get_voi_val(name, meta, self.driver._remote_cons)
+                for name, meta in self.driver._cons.items()
+            ]
+        )
         return F, G
