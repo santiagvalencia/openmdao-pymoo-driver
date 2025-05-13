dc9c47b04f4fc0ab4b349f000266ae6957b1d407
Santiago Valencia <42377578+santiagvalencia@users.noreply.github.com>
2025-05-13 16:04:25 -0400

    Add pymoo_driver.py and evaluator.py

---
+++ pymoo_driver/driver.py
@@ -0,0 +1,98 @@
+import numpy as np
+from openmdao.core.driver import Driver
+from pymoo.optimize import minimize
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.termination import get_termination
+from pymoo.core.termination import Termination
+
+from .evaluator import OpenMDAOEvaluator
+
+
+class PymooDriver(Driver):
+    """
+    OpenMDAO driver using pymoo for evolutionary multi-objective optimization.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.supports["optimization"] = True
+        self.supports["multiple_objectives"] = True
+        self.supports["inequality_constraints"] = True
+        self.supports["equality_constraints"] = True
+        self.supports["integer_design_vars"] = True
+        self.supports._read_only = True
+
+        self.options.declare(
+            "algorithm", default=NSGA2(pop_size=40), desc="pymoo algorithm instance"
+        )
+        self.options.declare(
+            "termination",
+            default=("n_gen", 100),
+            desc="pymoo Termination condition (tuple or Termination instance)",
+        )
+        self.options.declare(
+            "verbose",
+            default=True,
+            desc="Whether to run the pymoo minimization in verbose mode",
+        )
+
+    def _declare_options(self):
+        pass
+
+    def run(self):
+        self.result.reset()
+        self._check_for_missing_objective()
+        self._check_for_invalid_desvar_values()
+
+        evaluator = OpenMDAOEvaluator(self)
+
+        # Prepare problem dimensions and bounds
+        n_var = sum(meta["size"] for meta in self._designvars.values())
+        xl = np.concatenate(
+            [np.ravel(meta["lower"]) for meta in self._designvars.values()]
+        )
+        xu = np.concatenate(
+            [np.ravel(meta["upper"]) for meta in self._designvars.values()]
+        )
+        n_obj = len(self._objs)
+        n_constr = len(self._cons)
+
+        class Problem(ElementwiseProblem):
+            def __init__(self):
+                super().__init__(
+                    n_var=n_var, n_obj=n_obj, n_constr=n_constr, xl=xl, xu=xu
+                )
+                self._evaluator = evaluator
+
+            def _evaluate(self, x, out, *args, **kwargs):
+                F, G = self._evaluator.evaluate(x)
+                out["F"] = F
+                out["G"] = G
+
+        prob = Problem()
+
+        termination_opt = self.options["termination"]
+
+        res = minimize(
+            prob,
+            self.options["algorithm"],
+            termination=termination_opt
+            if isinstance(termination_opt, Termination)
+            else get_termination(*termination_opt),
+            verbose=self.options["verbose"],
+        )
+
+        self._apply_solution(res.X)
+        return False
+
+    def _apply_solution(self, x):
+        """Assign optimal design variable values and run the final model."""
+        i = 0
+        for name, meta in self._designvars.items():
+            size = meta["size"]
+            self.set_design_var(name, x[i : i + size])
+            i += size
+
+        self._run_solve_nonlinear()
---
+++ pymoo_driver/evaluator.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class OpenMDAOEvaluator:
+    """
+    Wraps an OpenMDAO problem so that it can be evaluated by pymoo.
+    """
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def set_design_vars(self, x):
+        """Assign values to OpenMDAO design variables from a flat numpy array."""
+        i = 0
+        for name, meta in self.driver._designvars.items():
+            size = meta["size"]
+            self.driver.set_design_var(name, x[i:i + size])
+            i += size
+
+    def evaluate(self, x):
+        """
+        Run the OpenMDAO model with design vector x and return objective and constraint values.
+        """
+        self.set_design_vars(x)
+
+        try:
+            self.driver._run_solve_nonlinear()
+        except Exception:
+            nF = len(self.driver._objs)
+            nG = len(self.driver._cons)
+            return np.full(nF, 1e20), np.full(nG, 1e20)
+
+        F = np.array([
+            self.driver._get_voi_val(name, meta, self.driver._remote_objs)
+            for name, meta in self.driver._objs.items()
+        ])
+        G = np.array([
+            self.driver._get_voi_val(name, meta, self.driver._remote_cons)
+            for name, meta in self.driver._cons.items()
+        ])
+        return F, G
